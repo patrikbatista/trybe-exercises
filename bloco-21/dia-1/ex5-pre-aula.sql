f81d92ff457f7f9d6775500c547e6a32293da6f5
patrik <pcr_batista@hotmail.com>
2022-05-30 20:44:40 -0300

    ex5 do course, trabalhando com datas

---
+++ bloco-21/dia-1/ex5-pre-aula.sql
@@ -0,0 +1,5 @@
+-- Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
+SELECT DATEDIFF( '2030-01-20', CURRENT_DATE());
+
+-- Para fixar, responda como seria possível encontrar as seguintes informações:
+SELECT TIMEDIFF('11:00:00', '10:25:45');
