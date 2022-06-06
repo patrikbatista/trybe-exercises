fdfdbcd03b4093ae5bdee8c3eb770c479659841a
patrik <pcr_batista@hotmail.com>
2022-06-06 17:01:55 -0300

    finaliza exercicio

--- bloco-22/dia-1/pos-aula-vivo/exImc/imc.js
+++ bloco-22/dia-1/pos-aula-vivo/exImc/imc.js
@@ -1,26 +1,35 @@
-const calcImc = (peso, altura) => {
-  const alturaAoQuadrado = altura ** 2;
-  const imc = peso / alturaAoQuadrado;
+const readline = require('readline-sync');
 
-  switch (imc) {
-    case imc < 18.5:
-      console.log('Abaixo do peso: "Magreza"');
-      break;
-    case imc >= 18.5 && imc <= 24.9:
-      console.log('Peso normal');
-      break;
-    case imc >= 25 && imc <= 29.9:
-      console.log('Sobrepeso');
-      break;
-    case imc >= 30 && imc <= 34.9:  
-      console.log('Obesidade grau I');
-      break;
-    case imc >= 35 && imc <= 39.9:
-      console.log('Obesidade grau II');
-      break;
-    case imc >= 40:
-      console.log('Obesidade grau III e IV');
+const calcImc = () => {
+  const peso = readline.questionFloat('Digite seu peso em kg: ');
+  const altura = readline.questionFloat('Digite sua altura em : '); 
+  const imc = peso / Math.pow(altura, 2)
+
+  if (imc < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
   }
+
+  if (imc >= 18.5 && imc < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+
+  if (imc >= 25 && imc < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+
+  if (imc >= 30 && imc < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+
+  if (imc >= 35 && imc < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }else {
+    console.log('Situação: Obesidade graus III e IV');}
 
 }
 
