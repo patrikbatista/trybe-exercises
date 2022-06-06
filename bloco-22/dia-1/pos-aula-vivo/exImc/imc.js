7768ec839465211f54e61e74926749b687ef176e
patrik <pcr_batista@hotmail.com>
2022-06-06 15:35:54 -0300

    refatora arquivo imc.js

--- bloco-22/dia-1/pos-aula-vivo/exImc/imc.js
+++ bloco-22/dia-1/pos-aula-vivo/exImc/imc.js
@@ -1,9 +1,29 @@
-const readline = require('readline-sync');
+const calcImc = (peso, altura) => {
+  const alturaAoQuadrado = altura ** 2;
+  const imc = peso / alturaAoQuadrado;
 
-const peso = readline.questionInt('Digite seu peso em kg: ');
-const altura = readline.questionInt('Digite sua altura em cm: ');
+  switch (imc) {
+    case imc < 18.5:
+      console.log('Abaixo do peso: "Magreza"');
+      break;
+    case imc >= 18.5 && imc <= 24.9:
+      console.log('Peso normal');
+      break;
+    case imc >= 25 && imc <= 29.9:
+      console.log('Sobrepeso');
+      break;
+    case imc >= 30 && imc <= 34.9:  
+      console.log('Obesidade grau I');
+      break;
+    case imc >= 35 && imc <= 39.9:
+      console.log('Obesidade grau II');
+      break;
+    case imc >= 40:
+      console.log('Obesidade grau III e IV');
+  }
 
-const alturaMetros = altura / 100;
-const alturaAoQuadrado = alturaMetros * alturaMetros;
-const imc = peso / alturaAoQuadrado;
-console.log(`${imc}`);
+}
+
+module.exports = {
+  calcImc
+}
