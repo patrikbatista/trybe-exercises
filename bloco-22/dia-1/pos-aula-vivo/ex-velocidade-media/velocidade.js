f74b97071529b7225df8bb27fddfb878b12b29ae
patrik <pcr_batista@hotmail.com>
2022-06-06 18:37:07 -0300

    faz uma alteracao no log do arquivo

--- bloco-22/dia-1/pos-aula-vivo/ex-velocidade-media/velocidade.js
+++ bloco-22/dia-1/pos-aula-vivo/ex-velocidade-media/velocidade.js
@@ -1,6 +1,6 @@
 const velocidadeMedia = (distancia, tempo) => {
   const res = distancia / tempo;
-  console.log(`Velocidade média: ' + ${res} + ' m/s`);
+  console.log(`Velocidade média: ${res} + ' m/s`);
 }
 
 module.exports = velocidadeMedia;
