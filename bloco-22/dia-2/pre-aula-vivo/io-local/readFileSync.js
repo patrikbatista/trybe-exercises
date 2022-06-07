29372ba9f0c62ea82c786dbc4db70d7ae41e9beb
patrik <pcr_batista@hotmail.com>
2022-06-07 16:00:45 -0300

    implementa outra maneira de se trabalhar arquivos de forma assincrona

--- bloco-22/dia-2/pre-aula-vivo/io-local/readFileSync.js
+++ bloco-22/dia-2/pre-aula-vivo/io-local/readFileSync.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
+// const fs = require('fs');
+
+// const nomeDoArquivo = 'meu-arquivo.txt';
+
+// try {
+//   const data = fs.readFileSync(nomeDoArquivo, 'utf8');
+//   console.log(data);
+// } catch (err) {
+//   console.error(`Erro ao ler o arquivo: ${err.path}`);
+//   console.log(err);
+// }
+
+const fs = require('fs').promises;
 
 const nomeDoArquivo = 'meu-arquivo.txt';
 
-try {
-  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(`Erro ao ler o arquivo: ${err.path}`);
-  console.log(err);
-}
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1); // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
+  });
