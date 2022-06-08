cec896929aeaeccd8b2c27543b37def47f8ae449
patrik <pcr_batista@hotmail.com>
2022-06-08 20:41:02 -0300

    finaliza, implementa a funcao que escreve no arquivo

--- bloco-22/dia-3/pos-aula-vivo/ex2/test/escreveArquivo.test.js
+++ bloco-22/dia-3/pos-aula-vivo/ex2/test/escreveArquivo.test.js
@@ -1,16 +1,21 @@
 const { expect } = require('chai');
+const fs = require('fs');
+const sinon = require('sinon');
 
 const escreveArquivo = require('../src/escreveArquivo')
 
 describe('Executa funçao escreveArquivo', () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
   describe('A resposta', () => {
     it('É uma string', () => {
-      const rest = escreveArquivo('arquivo.txt', 'Vqv trybe');
+      const resp = escreveArquivo('arquivo.txt', 'Vqv trybe');
       expect(resp).to.be.a('string');
     })
 
     it('retorna " ok"', () => {
-      const rest = escreveArquivo('arquivo.txt', 'Vqv trybe');
+      const resp = escreveArquivo('arquivo.txt', 'Vqv trybe');
       expect(resp).to.be.equal('ok');
     })
   })
