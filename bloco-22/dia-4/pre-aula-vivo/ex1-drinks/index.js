3dcd8ab440290e18d1d8d752ac9a65cabd4231b3
patrik <pcr_batista@hotmail.com>
2022-06-09 15:20:39 -0300

    altera estrutura de arquivos

--- bloco-22/dia-4/pre-aula-vivo/ex1-drinks/index.js
+++ bloco-22/dia-4/pre-aula-vivo/ex1-drinks/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes')
+
+app.use(express.json());
+
+app.use('/receitas', routes.receitas);
+app.use('/bebidas', routes.bebidas);
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
