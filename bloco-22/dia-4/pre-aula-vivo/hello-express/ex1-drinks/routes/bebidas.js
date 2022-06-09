6338d417afe5f5d330078e5d19e0308028ddd97f
patrik <pcr_batista@hotmail.com>
2022-06-09 15:09:00 -0300

    implementa bebidas.js

---
+++ bloco-22/dia-4/pre-aula-vivo/hello-express/ex1-drinks/routes/bebidas.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+
+const drinks = [
+	{ id: 1, name: 'Refrigerante Lata', price: 5.0 },
+	{ id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+	{ id: 3, name: 'Suco 300ml', price: 4.0 },
+	{ id: 4, name: 'Suco 1l', price: 10.0 },
+	{ id: 5, name: 'Cerveja Lata', price: 4.5 },
+	{ id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+const bebidas = Router();
+
+bebidas.get('/bebidas', (req, res) => {
+  res.status(200).json(drinks);
+});
+
+module.exports = bebidas;
