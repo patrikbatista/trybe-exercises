a0a630f89955cbfa78a032704ea1a62107a2afee
patrik <pcr_batista@hotmail.com>
2022-06-09 15:03:50 -0300

    implementa rota get recceitas

---
+++ bloco-22/dia-4/pre-aula-vivo/hello-express/ex1-drinks/routes/receitas.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+const receitas = Router();
+
+receitas.get('/receitas', (req, res) => {
+  res.status(200).json(recipes);
+});
+
+module.exports = receitas;
