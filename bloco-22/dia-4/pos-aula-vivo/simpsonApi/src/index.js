b2408b39cd479a5db2e8b7fc2defc4749cfb5573
patrik <pcr_batista@hotmail.com>
2022-06-09 21:56:17 -0300

    implementa getAllSimpson

--- bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/index.js
+++ bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const simpsons = require('./routes/simpsons');
+
 const app = express();
 
 
 app.use(express.json());
+
+app.use('/simpsons', simpsons);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
--- bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/routes/simpsons.js
+++ bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/routes/simpsons.js
@@ -1,21 +1,17 @@
 const { Router } = require('express');
 const fs = require('fs').promises;
 
-// const simpsonsList = require('../../simpsons.json');
-
 const simpsons = Router();
 
-// const simpsons = JSON.parse(simpsonsList);
-
 const getAll = async () => {
-  const simpsonsFile = await fs.readFile('../../simpsons.json');
+  const simpsonsFile = await fs.readFile(path.join(__dir,'simpsons.json'), 'utf-8');
   const simpsons = JSON.parse(simpsonsFile);
 }
 
 simpsons.get('/', async(req, res) => {
   try {
     const result = await getAll();
-    if (result === []) return res.status(404).json({message: 'Nao exite lista'});
+    if (!result) return res.status(500).json({message: 'Nao exite lista'});
     return res.status(200).json({result});
   } catch (error) {
     console.error(error.message);
