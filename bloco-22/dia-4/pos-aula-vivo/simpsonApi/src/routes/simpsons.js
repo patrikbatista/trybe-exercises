13ae8597bde6726caa93351bf0820e585e5483fa
patrik <pcr_batista@hotmail.com>
2022-06-10 11:35:41 -0300

    implementa funcao getSimpsonId' '

--- bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/routes/simpsons.js
+++ bloco-22/dia-4/pos-aula-vivo/simpsonApi/src/routes/simpsons.js
@@ -6,19 +6,47 @@ const path = require('path');
 const simpsons = Router();
 
 const getAll = async () => {
-  const simpsonsFile = await fs.readFile('./src/simpsons.json', 'utf-8');
 
+  const simpsonsFile = await fs.readFile('./src/simpsons.json', 'utf-8');
   const simpsons = JSON.parse(simpsonsFile);
+
   return simpsons;
 }
 
+const getSimpsomId = async (id) => {
+
+  const simpsonsFile = await fs.readFile('./src/simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(simpsonsFile);
+
+  const simpsonId = simpsons.find((simpson)=> +simpson.id === +id);
+  return simpsonId;
+}
+
 simpsons.get('/', async(req, res) => {
+
   try {
     const result = await getAll();
     if (!result) return res.status(500).json({message: 'Nao exite lista'});
     return res.status(200).json({result});
   } catch (error) {
     console.error(error.message);
+  }
+
+});
+
+simpsons.get('/:id', async(req, res) => {
+
+  try {
+    const { id } = req.params;
+    const simpsonFind = await getSimpsomId(id);
+    if(!simpsonFind) {
+      return res.status(404).json({ message: 'simpson not found' });
+    }
+
+    return res.status(200).json(simpsonFind);
+    
+  } catch (error) {
+    console.log(error.message);
   }
 });
 
