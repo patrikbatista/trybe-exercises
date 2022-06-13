5fb39e3be76b0d96e066795438aa1ff66b2045cb
patrik <pcr_batista@hotmail.com>
2022-06-13 18:07:07 -0300

    refatora controllers/propductCreate

--- bloco-22/dia-5/pos-aula-vivo/saleEx/src/controllers/productCreate.js
+++ bloco-22/dia-5/pos-aula-vivo/saleEx/src/controllers/productCreate.js
@@ -1,3 +1,7 @@
-module.exports = (req, res) => {
+const Product = require('../services');
+
+module.exports = async (req, res) => {
+  const product = req.body
+  await Product.postProduct(product);
   res.status(201).json({message: 'Venda cadastrada com sucesso'});
 };
