95340ae83df14713acf86994b5fb91db8c876a4a
patrik <pcr_batista@hotmail.com>
2022-06-13 16:25:13 -0300

    implementa middleware/productName

---
+++ bloco-22/dia-5/pos-aula-vivo/saleEx/src/middlewares/productNameValidate.js
@@ -0,0 +1,10 @@
+const schemas = require('../schemas');
+
+module.exports = (req, res, next) => {
+  const {productName} = req.body;
+  const result = schemas.productNameValidate(productName);
+  if (!result.response){
+    return res.status(result.status).json({message: result.message});
+  }
+  next();
+}
