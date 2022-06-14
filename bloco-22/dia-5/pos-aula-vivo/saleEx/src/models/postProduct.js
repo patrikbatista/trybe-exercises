ba3aca2dee0108626cb57f560489b91abcbc8848
patrik <pcr_batista@hotmail.com>
2022-06-14 12:21:47 -0300

    refatora

--- bloco-22/dia-5/pos-aula-vivo/saleEx/src/models/postProduct.js
+++ bloco-22/dia-5/pos-aula-vivo/saleEx/src/models/postProduct.js
@@ -1,6 +1,10 @@
 const fs = require('fs/promises');
+const path = require('path');
 
 module.exports = async (product) => {
-  const newProduct = await fs.writeFile('./data/salesProducts.json', JSON.stringify({product}));
-  return newProduct;
-}
+
+  // const data = await fs.readFile('./data/salesProducts.json', 'utf-8');
+  // data.push(product);
+  await fs.writeFile('./data/salesProducts.json', JSON.stringify(product));
+
+}        
