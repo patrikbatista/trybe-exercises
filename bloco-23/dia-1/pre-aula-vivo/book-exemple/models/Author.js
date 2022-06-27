0cb7fb51386dfd955bc83534b41cd2b2f8d9dad7
patrik <pcr_batista@hotmail.com>
2022-06-27 15:33:59 -0300

    refactor: refatora Author.js

--- bloco-23/dia-1/pre-aula-vivo/book-exemple/models/Author.js
+++ bloco-23/dia-1/pre-aula-vivo/book-exemple/models/Author.js
@@ -1,6 +1,5 @@
- const connection = require('./connection');
+const connection = require('./connection');
 
-// Cria uma string com o nome completo do autor
 const getNewAuthor = ({id, firstName, middleName, lastName}) => {
 
   // Note que `Boolean` é uma função que recebe um parâmetro e retorna true ou false
@@ -17,22 +16,23 @@ const getNewAuthor = ({id, firstName, middleName, lastName}) => {
     fullName,
     };
   };
-  
-  // Converte o nome dos campos de snake_case para camelCase
-  const serialize = (authorData) => ({
-    id: authorData.id,
-    firstName: authorData.first_name,
-    middleName: authorData.middle_name,
-    lastName: authorData.last_name});
-  
-  //  // Busca todos os autores do banco.
-   const getAll = async () => {
-   	const [authors] = await connection.execute(
-   		'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
-   	);
-        return authors.map(serialize).map(getNewAuthor);
-   };
-  
-   module.exports = {
-   	getAll,
-   };
+
+// Converte o nome dos campos de snake_case para camelCase
+const serialize = (authorData) => ({
+	id: authorData.id,
+	firstName: authorData.first_name,
+	middleName: authorData.middle_name,
+	lastName: authorData.last_name});
+
+// Busca todas as pessoas autoras do banco.
+
+const getAll = async () => {
+	const [authors] = await connection.execute(
+		'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
+	);
+	return authors.map(serialize).map(getNewAuthor);
+};
+
+module.exports = {
+	getAll,
+};
