d85fd2acb003b9ed17b80ecc4346d9eeb4702b67
patrik <pcr_batista@hotmail.com>
2022-06-27 13:45:30 -0300

    implementa funcao getNewAuthor

--- bloco-23/dia-1/pre-aula-vivo/ model-example/models/Author.js
+++ bloco-23/dia-1/pre-aula-vivo/ model-example/models/Author.js
@@ -1,5 +1,22 @@
 const connection = require('./connection');
 
+const getNewAuthor = ({id, firstName, middleName, lastName}) => {
+
+  // Note que `Boolean` é uma função que recebe um parâmetro e retorna true ou false
+  // nesse caso, se middle_name for `undefined` ou uma string vazia o retorno será `false`
+    const fullName = [firstName, middleName, lastName]
+      .filter(Boolean)
+      .join(' ');
+  
+    return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    fullName,
+    };
+  };
+
 // Converte o nome dos campos de snake_case para camelCase
 const serialize = (authorData) => ({
 	id: authorData.id,
@@ -13,7 +30,7 @@ const getAll = async () => {
 	const [authors] = await connection.execute(
 		'SELECT id, first_name, middle_name, last_name FROM model_example.authors;',
 	);
-	return authors.map(serialize);
+	return authors.map(serialize).map(getNewAuthor);
 };
 
 module.exports = {
