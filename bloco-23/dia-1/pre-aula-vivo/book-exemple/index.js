d59b04387e1705094aec5c5e6bad108c8e9cfa05
patrik <pcr_batista@hotmail.com>
2022-06-27 15:20:22 -0300

    feat: implementa get books

--- bloco-23/dia-1/pre-aula-vivo/book-exemple/index.js
+++ bloco-23/dia-1/pre-aula-vivo/book-exemple/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const Book = require('./models/Book');
+
+
+app.get('/books', async (_req, res) => {
+	const book = await Book.getAll();
+
+	res.status(200).json(book);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
