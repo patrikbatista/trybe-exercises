4067efc2eaacd09e8055110022f0cb0c55e736bf
patrik <pcr_batista@hotmail.com>
2022-06-27 15:44:52 -0300

    feat: implementa rota de authorId

--- bloco-23/dia-1/pre-aula-vivo/book-exemple/index.js
+++ bloco-23/dia-1/pre-aula-vivo/book-exemple/index.js
@@ -5,10 +5,20 @@ const app = express();
 const Book = require('./models/Book');
 const Author = require('./models/Author');
 
-app.get('/author', async (_req, res) => {
+app.get('/authors', async (_req, res) => {
 	const authors = await Author.getAll();
 
 	res.status(200).json(authors);
+});
+
+app.get('/authors/:id', async (req, res) => {
+	const { id } = req.params;
+
+	const author = await Author.findById(id);
+
+	if (!author) return res.status(404).json({ message: 'Not found' });
+
+	res.status(200).json(author);
 });
 
 app.get('/books', async (_req, res) => {
@@ -16,6 +26,8 @@ app.get('/books', async (_req, res) => {
 
 	res.status(200).json(books);
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 
