b2b56340b74dbbb349737ca3a52d94632df49d40
patrik <pcr_batista@hotmail.com>
2022-06-28 20:16:32 -0300

    implementa validateBodyEdit

--- bloco-23/dia-2/pos-aula-vivo/cep-lookup/src/schemas/cepSchemas.js
+++ bloco-23/dia-2/pos-aula-vivo/cep-lookup/src/schemas/cepSchemas.js
@@ -1,17 +1,39 @@
 const cepValidate = require('@brazilian-utils/is-valid-cep');
+const Joi = require('joi');
 
 const isValideCep = (cep) => {
   const isValid = cepValidate(cep);
   if (!isValid) {
     const error = { 
       status: 400, 
-      message: { error: { code: 'invalidData', message: 'CEP inválido' } }, 
+      message: 'CEP inválido', 
     };
     throw error;
   }
   return true;
 };
 
+const bodyAddSchema = Joi.object({
+  cep: Joi.string(),
+  logradouro: Joi.string().required(),
+  bairro: Joi.string().required(),
+  localidade: Joi.string().required(),
+  uf: Joi.string().required(),
+}).required();
+
+const validateBodyEdit = async (value) => {
+  const result = await bodyAddSchema.validateAsync(value);
+  if (!result) {
+    const error = { 
+      status: 401, 
+      message: 'Dados inválidos', 
+    };
+    throw error;
+  }
+  return result;
+};
+
 module.exports = {
   isValideCep,
+  validateBodyEdit,
 };
