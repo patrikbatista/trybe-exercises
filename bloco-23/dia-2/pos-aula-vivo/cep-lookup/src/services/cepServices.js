6f18bc7754db4145ea2c0d3accaa7a456ebf58f7
patrik <pcr_batista@hotmail.com>
2022-06-28 15:34:59 -0300

    feat: implementa funcao que verifica se o cep existe no db

---
+++ bloco-23/dia-2/pos-aula-vivo/cep-lookup/src/services/cepServices.js
@@ -0,0 +1,17 @@
+const cepModel = require('../models/cepModel');
+
+const cepExist = async (cep) => {
+  const result = await cepModel.getCep(cep);
+  if (!result) {
+    const error = { 
+      status: 400,
+      message: { error: { code: 'notFound', message: 'CEP não encontrado' } },
+    };
+    throw error;
+  }
+  return result;
+};
+
+module.exports = {
+  cepExist,
+};
