ba08377ae7c2080380610673f238426113062fa4
patrik <pcr_batista@hotmail.com>
2022-06-28 08:31:19 -0300

    feat: implementa middleware de erro

--- bloco-23/dia-2/pos-aula-vivo/cep-lookup/src/index.js
+++ bloco-23/dia-2/pos-aula-vivo/cep-lookup/src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+require('express-async-errors');
+
+const handlerError = require('./middlewares/handlerError');
 
 const app = express();
+app.use(handlerError);
 
 app.listen(3000, () => console.log('listening on port 3000'));
