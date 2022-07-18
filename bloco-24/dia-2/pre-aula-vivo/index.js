5f121d82b19be171b04ae5802e4e5d2da05a1a9d
patrik <pcr_batista@hotmail.com>
2022-07-18 17:15:09 -0300

    rafactor: rota employee id

--- bloco-24/dia-2/pre-aula-vivo/index.js
+++ bloco-24/dia-2/pre-aula-vivo/index.js
@@ -9,7 +9,7 @@ app.get('/employees/:id', async (req, res) => {
     const { id } = req.params;
     const employee = await Employee.findOne({
         where: { id },
-        include: [{ model: Address, as: 'addresses' }],
+        include: [{ model: Address, as: 'addresses', attributes: { exclude: ['employeeId']}}],
       });
 
     if (!employee)
