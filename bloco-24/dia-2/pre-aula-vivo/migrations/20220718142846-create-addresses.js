9cd38a8d435e2f9ac675d168ee1e9897d4a29021
patrik <pcr_batista@hotmail.com>
2022-07-18 11:32:45 -0300

    feat: implementa migrations que cria a tabela address

--- bloco-24/dia-2/pre-aula-vivo/migrations/20220718142846-create-addresses.js
+++ bloco-24/dia-2/pre-aula-vivo/migrations/20220718142846-create-addresses.js
@@ -1,21 +1,43 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      street: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      employeeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        // Configuram o que deve acontecer ao atualizar ou excluir um usuário
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'employee_id',
+        // Informa que o campo é uma Foreign Key (Chave estrangeira)
+        references: {
+          // Informa a tabela da referência da associação
+          model: 'Employees',
+          // Informa a coluna da referência que é a chave correspondente
+          key: 'id',
+        },
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Addresses');
+  },
 };
