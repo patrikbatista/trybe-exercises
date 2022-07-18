b9db6fac35ea62f498889a34a6ad5318eaab4880
patrik <pcr_batista@hotmail.com>
2022-07-18 12:30:48 -0300

    feat: implementa rota eployee id

--- bloco-24/dia-2/pre-aula-vivo/index.js
+++ bloco-24/dia-2/pre-aula-vivo/index.js
@@ -4,6 +4,24 @@ const { Address, Employee } = require('./models');
 
 const app = express();
 
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employee = await Employee.findOne({
+        where: { id },
+        include: [{ model: Address, as: 'addresses' }],
+      });
+
+    if (!employee)
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
+
 app.get('/employees', async (_req, res) => {
   try {
     const employees = await Employee.findAll({
