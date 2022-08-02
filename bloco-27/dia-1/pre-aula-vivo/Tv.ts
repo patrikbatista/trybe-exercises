586f97daff12c31b83f89b30951f0358aab95530
patrik <pcr_batista@hotmail.com>
2022-08-02 16:13:28 -0300

    feat: implementa a classe Tv

---
+++ bloco-27/dia-1/pre-aula-vivo/Tv.ts
@@ -0,0 +1,23 @@
+export default class Tv {
+  private brand: string;
+  private size: number;
+  private resolution: string;
+  private connections: string[];
+  private connectedTo?: string;
+
+  constructor(brand: string, size: number, resolution: string, connection: string[]) {
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resolution;
+    this.connections = connection;
+  }
+
+  turnOn(): void {
+    console.log(
+      `Tv ${this.brand}, 
+      size ${this.size}, 
+      resolution ${this.resolution}, 
+      connections ${this.connections} 
+    `);
+  }  
+}
